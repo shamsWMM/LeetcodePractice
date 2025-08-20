f68d333063b9c9ad6df1e87b7bf77913461779e9
shamsWMM <shams.alsaegh@gmail.com>
2025-08-20 14:09:23 +1000

    better memory use

--- 8. String to Integer/Submission01.c
+++ 8. String to Integer/Submission01.c
@@ -2,30 +2,31 @@
 #include <stdint.h>
 
 int myAtoi(char *s);
-int processLargeNumber(int index, int tenthDigit, int num); 
+int processLargeNumber(int s, int next, int num);
 
 int myAtoi(char *s)
 {
-    int index = 0, sign = 0, current;
-    int digits[200] = {0};
+    int index = 0, sign = 0, result = 0, current;
 
     // identify valid digits
-    while (s && *s)
+    while (s && *s && index < 9)
     {
         current = *s;
-        if (!sign && !index && current == 32); // leading space
-        else if (!sign && !index && (current == 43 || current == 45)) // leading sign
+        if (!sign && !index && current == ' ') // leading space
+            ;
+        else if (!sign && !index && (current == '-' || current == '+')) // leading sign
         {
-            sign = current;
+            sign = 44 - current;
         }
-        else if(!index && current == 48) // leading zero
+        else if (!index && current == '0') // leading zero
         {
             index = 0;
-            sign = sign == 45 ? 45 : 43; // flag progress
+            sign = sign == -1 ? sign : 1; // flag progress
         }
-        else if(current >= 48 && current <= 57) // valid character
+        else if (current >= '0' && current <= '9') // valid character
         {
-            digits[index++] = current - 48;
+            result = 10 * result + current - '0';
+            index++;
         }
         else // invalid character
         {
@@ -34,44 +35,39 @@ int myAtoi(char *s)
         s++;
     }
 
-    // process number up to max
-    int result = 0;
-    for (int i = 0; i < index && i < 9; i++)
-    {
-        result = 10 * result + digits[i];
-    }
-
-    // add sign
-    result = sign == 45 ? -1 * result : result;
+    result = !sign ? result : sign * result;
 
     // check boundary cases
-    if (index > 9)
+    current = *s;
+    if (index == 9 && current >= '0' && current <= '9')
     {
-        result = processLargeNumber(index, digits[9], result);
+        current -= '0';
+        int nextExits = *(s + 1) >= '0' && *(s + 1) <= '9';
+        result = processLargeNumber(current, nextExits, result);
     }
 
     return result;
 }
 
-int processLargeNumber(int index, int tenthDigit, int num)
+int processLargeNumber(int current, int nextExists, int num)
 {
     int max = 214748364;
-    if ((index > 10 && num > 0) || num > max || (num == max && tenthDigit > 7))
+    if (
+        nextExists ||
+        num > max ||
+        num < -max ||
+        (num == max && current > 7) ||
+        (num == -max && current > 8))
     {
-        return max * 10 + 7;
+        return num < 0 ? -max * 10 - 8 : max * 10 + 7;
     }
-    else if(index > 10 || num < - max || (num == - max && tenthDigit > 8)
-    )
-    {
-        return - max * 10 - 8;
-    }
-    tenthDigit = num < 0 ? -1 * tenthDigit : tenthDigit;
-    return num * 10 + tenthDigit;
+
+    return num < 0 ? num * 10 - current : num * 10 + current;
 }
 
 int main()
 {
-    char* num = "1337c0d3";
+    char *num = "2147483646";
     int result = myAtoi(num);
     printf("Result: %d\n", result);
     return 0;
